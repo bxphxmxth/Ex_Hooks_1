2e2636f3a5d2af84eae78d72dfd0f64dd2bd34f8
bxphxmxth <fushs@hotmail.fr>
2021-06-29 15:06:48 +0200

    end2

--- src/components/Personnages.js
+++ src/components/Personnages.js
@@ -5,7 +5,8 @@ class Personnages extends React.Component{
      render(){
           return(
                <div>
-                    <p>{this.props.nasila.prenom}</p>
+                    <p>Je m'appelle {this.props.nasila.prenom} {this.props.nasila.nom} , j'ai {this.props.nasila.age} ans et j'habite {this.props.nasila.commune}   </p>
+                    <button onClick={this.props.rename}>Rename</button>
                </div>
           )
      }
